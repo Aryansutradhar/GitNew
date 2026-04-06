4f15379fa11d7a09311d7db51c703ec6589f85b2
Aryan Sutradhar <147043873+Aryansutradhar@users.noreply.github.com>
2026-04-06 21:20:09 +0530

    Add files via upload

---
+++ Task 2/even or odd .py
@@ -0,0 +1,7 @@
+N=int(input("enter your number"))
+
+if N % 2==0:
+    print(f"{N} is an even number")
+else:
+    print(f"{N} is an odd number")
+10
---
+++ Task 2/sum of integer.py
@@ -0,0 +1,7 @@
+def is_sum():  
+    total=0
+    for i in range(1,51):
+        total+=i
+    return total
+total=is_sum()
+print(total)
